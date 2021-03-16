a6b1eb8cea12b946f3ee797c605fcb5a3c021c58
Seungjin Kim <skim@proove.com>
2021-03-16 06:08:37 -0700

    ADD: first test for decreasing amount

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,6 +1,17 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
-  test "begin the journey of refactoring" do
+  test "should lower both sellIn and quality after one day" do
+    startCount = 3
+
+    items =
+      GildedRose.update_quality([%Item{name: "foo", sell_in: startCount, quality: startCount}])
+      |> IO.inspect()
+
+    [%{name: name, sell_in: sell_in, quality: quality} | _] = items
+
+    assert name == "foo"
+    assert sell_in == startCount - 1
+    assert quality == startCount - 1
   end
 end
