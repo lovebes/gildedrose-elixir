155089bec4caca754dd3242ae0a143b64a4abd42
Seungjin Kim <skim@proove.com>
2021-03-16 06:13:31 -0700

    ADD: test for lower quality for when sell in is < 0

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -6,12 +6,27 @@ defmodule GildedRoseTest do
 
     items =
       GildedRose.update_quality([%Item{name: "foo", sell_in: startCount, quality: startCount}])
-      |> IO.inspect()
 
     [%{name: name, sell_in: sell_in, quality: quality} | _] = items
 
     assert name == "foo"
     assert sell_in == startCount - 1
     assert quality == startCount - 1
+  end
+
+  test "should lower quality twice as fast if sell by date has passed" do
+    start_sell_in = 1
+    start_quality = 5
+    items = [%Item{name: "foo", sell_in: start_sell_in, quality: start_quality}]
+
+    updated_items =
+      items
+      |> GildedRose.update_quality()
+      |> GildedRose.update_quality()
+
+    [%{name: name, sell_in: sell_in, quality: quality} | _] = updated_items
+
+    assert sell_in == start_sell_in - 1 - 1
+    assert start_quality = start_quality - 1 - 2
   end
 end
