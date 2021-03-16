9012d6506a2a6a9862a45b8485c8fba991c8bb95
Seungjin Kim <skim@proove.com>
2021-03-16 06:27:03 -0700

    ADD: test for list of items update check

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -66,4 +66,39 @@ defmodule GildedRoseTest do
 
     assert start_quality = start_quality + 1 + 1
   end
+
+  test "should iterate and affect logic correctly to list of items" do
+    items = [
+      %Item{name: @aged_brie, sell_in: 1, quality: 2},
+      %Item{name: "foo", sell_in: 3, quality: 4},
+      %Item{name: @sulfras, sell_in: 5, quality: @sulfras_quality},
+      %Item{name: @backstage, sell_in: 7, quality: 8}
+    ]
+
+    updated_items =
+      items
+      |> GildedRose.update_quality()
+
+    for %Item{name: name, sell_in: sell_in, quality: quality} = item <- updated_items do
+      start_item = items |> Enum.find(nil, fn %Item{name: target_name} -> target_name == name end)
+
+      case name do
+        @aged_brie ->
+          assert sell_in == start_item.sell_in - 1
+          assert quality == start_item.quality + 1
+
+        @sulfras ->
+          assert sell_in == start_item.sell_in
+          assert quality == @sulfras_quality
+
+        @backstage ->
+          assert sell_in == start_item.sell_in - 1
+          assert quality == start_item.quality + 2
+
+        _ ->
+          assert sell_in == start_item.sell_in - 1
+          assert quality == start_item.quality - 1
+      end
+    end
+  end
 end
